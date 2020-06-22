aa4b2010feb83e167853af81e08215c9c3df7545
SupriyaK8 <65591250+SupriyaK8@users.noreply.github.com>
2020-06-22 09:42:30 +0530

    Update SellerService.java

--- src/main/java/com/management/UserMS/service/SellerService.java
+++ src/main/java/com/management/UserMS/service/SellerService.java
@@ -4,25 +4,17 @@ import java.util.regex.Pattern;
 
 import javax.naming.InvalidNameException;
 
-//import org.hibernate.annotations.common.util.impl.Log_.logger;
-import org.omg.CORBA.UserException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-import com.management.UserMS.controller.SellerController;
-import com.management.UserMS.dto.BuyerDTO;
 import com.management.UserMS.dto.SellerDTO;
-import com.management.UserMS.entity.Buyer;
+
 import com.management.UserMS.entity.Seller;
 import com.management.UserMS.repository.SellerRepository;
-
 @Service
 public class SellerService {
-	private static final Logger logger = LoggerFactory.getLogger(SellerController.class);
+
 	@Autowired
 	SellerRepository sellerRepo;
 	
@@ -31,19 +23,14 @@ public class SellerService {
 
 	
 	
-	public void sellerRegistration(SellerDTO sellerDTO) throws Exception {
-		logger.info("Buyer details are going to validate");
-		sellerValidation(sellerDTO);
-		logger.info("Buyer details are validated successfully");
-		Seller sellerEntity = new Seller();
-		BeanUtils.copyProperties(sellerDTO, sellerEntity);
-		sellerRepo.save(sellerEntity);
-		logger.info("Buyer details are saved in DB successfully");
-		
+	public void sellerRegisterion(SellerDTO sellerDTO) throws Exception {
+		validateSeller(sellerDTO);
+		Seller se=sellerDTO.createEntity();
+		sellerRepo.save(se);
 	}
 	
-	private void sellerValidation(SellerDTO sellerDTO) throws Exception {
-		
+	private void validateSeller(SellerDTO sellerDTO) throws Exception {
+		// TODO Auto-generated method stub
 		
 		if(!isValidName(sellerDTO.getName()))
 			throw new InvalidNameException("Invalid Name");
@@ -61,7 +48,7 @@ public class SellerService {
 	}
 
 	private boolean isAlreadyEmailIdExist(String email) {
-	
+		// TODO Auto-generated method stub
 		Seller seller=sellerRepo.findByEmail(email);
 		if (seller!=null)
 			return false;
@@ -69,7 +56,7 @@ public class SellerService {
 	}
 
 	private boolean isAlreadyPhoneNumberExist(String phoneNumber) {
-		
+		// TODO Auto-generated method stub
 		Seller seller = sellerRepo.findByPhoneNumber(phoneNumber);
 		if (seller!=null)
 			return false;
@@ -77,43 +64,40 @@ public class SellerService {
 	}
 
 	private boolean isvalidPassword(String password) {
-		
+		// TODO Auto-generated method stub
 		return Pattern.matches("(?=.\\d)(?=.[a-z])(?=.[A-Z])(?=.[!@#$%^&*]).{7,20}$",password);
 	}
 
 	private boolean isValidPhoneNumber(String phoneNumber) {
-	
+		// TODO Auto-generated method stub
 		return Pattern.matches("^\\d{10}$", phoneNumber);
 	}
 
 	private boolean isValidEmail(String email) {
-		
+		// TODO Auto-generated method stub
 		return Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$",email);
 	}
 
 	private boolean isValidName(String name) {
-		
+		// TODO Auto-generated method stub
 		return Pattern.matches("^[a-zA-Z]+[-a-zA-Z\\s]+([-a-zA-Z]+)$", name);
 	}
 
 	
-	public void sellerLogin(SellerDTO sellerDTO) throws Exception {
+	public boolean sellerLogin(SellerDTO sellerDTO) throws Exception {
 
-		Seller sellerEntity = sellerRepo.findByEmail(sellerDTO.getEmail());
-		if (sellerEntity != null) {
-			if (sellerEntity.getPassword().equals(sellerEntity.getPassword())) {
-				
+		Seller seller = sellerRepo.findByEmail(sellerDTO.getEmail());
+		if (seller != null) {
+			if (seller.getPassword().equals(sellerDTO.getPassword())) {
+			return true;
 			} else {
-				throw new Exception("sellerLogin.INVALID_PASSWORD");
+				throw new Exception("BuyerLogin:Invalid Password");
 			}
-
-		} else {
-			throw new Exception("sellerLogin.INVALID_EMAILID");
 		}
-
+		return false;
 	}
 	
-	public boolean sellerDeactivation(SellerDTO sellerDTO) throws Exception {
+	public boolean deactivateSeller(SellerDTO sellerDTO) throws Exception {
 
 		Seller seller = sellerRepo.findByEmail(sellerDTO.getEmail());
 		if (seller != null) {
